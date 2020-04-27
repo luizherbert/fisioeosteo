2386855885e53101e8de9c769fd17af1824f042e
Luiz Herbert <luiz_herbert@hotmail.com>
2020-04-26 23:17:53 -0300

    Primeira versão

---
+++ js/principal.js
@@ -0,0 +1,3 @@
+TweenMax.staggerFrom(".quadrado1", 1, {x:100, opacity:0}, 0.4);
+TweenMax.staggerFrom(".quadrado2", 1, {x:-100, opacity:0}, 0.4);
+TweenMax.staggerFrom(".texto", 1, {x:-50, opacity:0}, 0.4);
