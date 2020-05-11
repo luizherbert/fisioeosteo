2e516ea6bc44719b72e9af0910910903339d7f9d
Luiz Herbert <luiz_herbert@hotmail.com>
2020-05-11 00:08:12 -0300

    fix layout

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,20 @@
 TweenMax.staggerFrom(".quadrado1", 1, {x:100, opacity:0}, 0.4);
 TweenMax.staggerFrom(".quadrado2", 1, {x:-100, opacity:0}, 0.4);
 TweenMax.staggerFrom(".texto", 1, {x:-50, opacity:0}, 0.4);
+
+const linkTratamento = document.getElementById("lnkTratamento");
+const lnkLocalizacao = document.getElementById("lnkLocalizacao");
+const lnkContato     = document.getElementById("lnkContato");
+
+
+linkTratamento.addEventListener("click", () => {
+    gsap.to(window, {duration: 1, scrollTo: ".sc-tratamento-osteopatia"});
+});
+
+lnkLocalizacao.addEventListener("click", () => {
+    gsap.to(window, {duration: 1, scrollTo: ".sc-localizacao"});
+});
+
+lnkContato.addEventListener("click", () => {
+    gsap.to(window, {duration: 1, scrollTo: ".sc-contato"});
+});
